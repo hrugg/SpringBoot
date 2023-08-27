59145138fae08578cfe18c5770d7f1d6e101a4f6
Hank Rugg <hrugg@carroll.edu>
2023-08-27 10:48:34 -0600

    Form Validation

--- src/main/java/edu/carroll/cs389/web/controller/IndexController.java
+++ src/main/java/edu/carroll/cs389/web/controller/IndexController.java
@@ -1,11 +1,11 @@
-package edu.carroll.cs389;
+package edu.carroll.cs389.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
+public class IndexController {
+    @GetMapping("/")
     public String index() {
         return "index";
     }
